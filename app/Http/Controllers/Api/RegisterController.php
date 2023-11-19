33c61c82ac52112165caaf109af4f2d51a929e40
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-19 14:18:37 -0600

    Añadiendo rutas para login y registro de usuarios

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = new User;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        if($request->employee_id){
+            $user->employee_id = $request->employee_id;
+        }
+        else {
+            $user->employee_id = null;
+        }
+        $user->user_role = $request->user_role;
+        $user->save();
+        return response()->json([
+            'Message' => 'Usuario creado con exito'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
+
+Route::post('register', [
+    App\Http\Controllers\Api\RegisterController::class,
+    'register'
+]);
