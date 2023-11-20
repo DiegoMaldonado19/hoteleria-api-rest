3d94ca38f2d8b27c0f26499e2fc7dfb126fb0841
Diego Maldonado <dj_maldonado19@hotmail.es>
2023-11-20 09:57:21 -0600

    Añadiendo rutas finales, en teoria deberian funcionar sin problemas, TODO: testear cada ruta

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use  App\Http\Controllers\Api\v1\EmployeeController as EmployeeV1;
+use  App\Http\Controllers\Api\v1\EmployeeRoleController as EmployeeRoleV1;
+use  App\Http\Controllers\Api\v2\ReservationController as ReservationV1;
+use  App\Http\Controllers\Api\v1\RoomController as RoomV1;
+use  App\Http\Controllers\Api\v1\RoomTypeController as RoomTypeV1;
+use  App\Http\Controllers\Api\v1\TaskController as TaskV1;
+use  App\Http\Controllers\Api\v1\TransactionController as TransactionV1;
+use  App\Http\Controllers\Api\v1\TransactionTypeController as TransactionTypeV1;
+use  App\Http\Controllers\Api\v1\UserController as UserV1;
+use  App\Http\Controllers\Api\v1\UserRoleController as UserRoleV1;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +24,48 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+/* Rutas para la version V1 */
+Route::apiResource('v1/employee', EmployeeV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/employee-role', EmployeeRoleV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/reservation', ReservationV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/room', RoomV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/room-type', RoomTypeV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/task', TaskV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/transaction', TransactionV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/transaction-type', TransactionTypeV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/user', UserV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+
+Route::apiResource('v1/user-role', UserRoleV1::class)
+->only(['index', 'store', 'show', 'update', 'destroy'])
+->middleware('auth:sanctum');
+/* Final de Rutas para la version V1 */
 
 Route::post('login', [
     App\Http\Controllers\Api\LoginController::class,
